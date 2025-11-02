63ac5d97771121155a386255f43178fc37a370b1
vineeth <vineethvineeth614@gmail.com>
2025-11-02 13:51:10 -0600

    Sample API's in productservice

---
+++ src/main/java/com/scaler/productservice1/controllers/SampleController.java
@@ -0,0 +1,41 @@
+package com.scaler.productservice1.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//host Rest HTTP APIs.
+
+@RequestMapping("/sample")
+/*
+when mapped/called or used /sample, point it to RequestMapping
+for amazon
+amazon.in/orders => Ordercontroller
+
+amazon.in/payment => PaymentController
+
+amazon.in/card => CartController
+ */
+
+public class SampleController {
+
+    @GetMapping("/hello/{name}/{times}")
+    public String hello(@PathVariable("name") String name,
+                        @PathVariable("times") int x) {
+        String s= "";
+
+        for (int i = 0; i < x; i++) {
+            s+= "Hello" + name + "! <br>";
+        }
+        return s;
+    }
+
+    @GetMapping("/hi")
+    public String hi() {
+        return "Hi";
+    }
+
+    //hello is endpoint and /hi is end point
+}
